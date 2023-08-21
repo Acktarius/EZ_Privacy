2238c25ddd1acad1bad38e50fa4ebd93df57c4b4
Acktarius <94026250+Acktarius@users.noreply.github.com>
2023-08-20 21:29:36 -0400

    Update ez_privacy.sh

--- ez_privacy.sh
+++ ez_privacy.sh
@@ -73,7 +73,7 @@ s=$?
 if [[ $s -ne 0 ]]; then echo "error during apt update"; sleep 2; exit; fi
 sudo apt-get install flatpak
 s=$?
-if [[ $s -ne 0 ]]; then echo "error during apt upodate"; sleep 2; exit; fi
+if [[ $s -ne 0 ]]; then echo "error during flatpak installation"; sleep 2; exit; fi
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 s=$?
 if [[ $s -ne 0 ]]; then echo "error during repo upodate"; sleep 2; exit; fi
